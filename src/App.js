92f6a4341920316da75be2db0108eea5c2734926
Hernando Abella <hernandoabella@gmail.com>
2022-12-08 23:23:58 -0500

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  // Constructor 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+      DataisLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          items: json,
+          DataisLoaded: true
+        });
+      })
+  }
+
+  render() {
+    const { DataisLoaded, items } = this.state;
+    if(!DataisLoaded) return <div>
+      <h1>Por favor espera un momento... </h1>
+    </div>;
+    return (
+      <div className="App">
+        <h1 className="titulo">Datos obtenidos de la API</h1> {
+        items.map((item) => (
+          <ol key={item.id}>
+            <li><b>Nombre del Usuario:</b> {item.username}<br /></li>
+            <li><b>Nombre Completo:</b> {item.name}<br /></li>
+            <li><b>Correo Electrónico:</b> {item.email}<br /></li>
+            {/* Correo Electrónico: {item.address} */}
+
+          </ol>
+        ))
+      }
+      </div>
+    );
+  }
 }
 
 export default App;
